7176111521a6027e0cddb530f4660779be73b51f
mokhif <dunosixen@gmail.com>
2026-05-31 13:59:35 +0100

    saving my local changes

--- backend/src/data-source.ts
+++ backend/src/data-source.ts
@@ -2,7 +2,12 @@ import "reflect-metadata";
 import "dotenv/config";
 import { DataSource } from "typeorm";
 import { User } from "./modules/users/entities/user.entities";
-
+import { Shop } from "./modules/shop/shop.entities";
+import { Category } from "./modules/category/category.entities";
+import { Service } from "./modules/services/service.entities";
+import { Review } from "./modules/reviews/review.entities";
+import { Photo } from "./modules/photos/photo.entities";
+import { Booking } from "./modules/booking/booking.entities";
 export const AppDataSource = new DataSource({
   type: "postgres",
   host: process.env.DB_HOST,
@@ -10,8 +15,9 @@ export const AppDataSource = new DataSource({
   username: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
-  synchronize: false, //true
+  synchronize: true ,//false, 
+
   logging: process.env.NODE_ENV === "development", // 'production'
-  entities: [User],
+  entities: [User,Shop,Category,Service,Review,Photo,Booking],
   migrations: ["src/migrations/*.ts"],
 });
