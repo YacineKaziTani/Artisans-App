967fc0304b97fb8a773cc3a9a442660a449f7426
YacineKaziTani <yacinekazitani266@gmail.com>
2026-06-02 14:50:55 +0100

    user datasource

--- backend/src/data-source.ts
+++ backend/src/data-source.ts
@@ -1,7 +1,6 @@
 import "reflect-metadata";
 import "dotenv/config";
 import { DataSource } from "typeorm";
-import { User } from "./modules/users/user.entities";
 import { Shop } from "./modules/shop/shop.entities";
 import { Category } from "./modules/category/category.entities";
 import { Photo } from "./modules/photos/photo.entities";
@@ -10,6 +9,7 @@ import { Product } from "./modules/products/product.entities";
 import { Review } from "./modules/reviews/review.entities";
 import { Service } from "./modules/services/service.entities";
 import { Booking } from "./modules/booking/booking.entities";
+import { User } from "./modules/users/entities/user.entities";
 
 export const AppDataSource = new DataSource({
   type: "postgres",
